0b282c076aa704248f97ec5d396b660ffad9cf87
charangnanagit59 <charangnana59@gmail.com>
2026-02-17 21:19:11 +0530

    Add files via upload

---
+++ OOPSBannerApp.java
@@ -0,0 +1,12 @@
+/**
+ * OOPSBannerApp
+ * Prints OOPS to console
+ */
+
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+
+}
